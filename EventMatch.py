651b407b599a5cb293c9b9c6cfaaa7e17a51f3c8
rheask8246 <60640612+rheask8246@users.noreply.github.com>
2021-06-14 09:48:17 -0700

    Initial commit

---
+++ EventMatch.py
@@ -0,0 +1,18 @@
+import os
+
+'''
+This program compares events between the standard and Kalman event reconstructions
+and outputs how many events match between them.
+Author: Rhea Senthil Kumar
+'''
+
+#CWD
+CWD = os.getcwd()
+
+#collects all ids from one tra file and stores in a list
+def id_list(filepath):
+    return
+
+#iterates through lists and counts how many ids are the same
+
+print("Current working directory: {0}".format(CWD))
