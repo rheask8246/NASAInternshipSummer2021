998bf61bf312f26629dfac14f5a68c59fa2e67c4
rheask8246 <rheask@berkeley.edu>
2021-06-14 10:08:41 -0700

    test argpase for event matching program

--- EventMatch.py
+++ EventMatch.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 '''
 This program compares events between the standard and Kalman event reconstructions
@@ -6,13 +7,25 @@ and outputs how many events match between them.
 Author: Rhea Senthil Kumar
 '''
 
-#CWD
-CWD = os.getcwd()
+#command line/variable setup
+#CWD = os.getcwd()
+parser = argparse.ArgumentParser(description='Count matching event ids from 2 event reconstruction files.')
+parser.add_argument("-s", "--standardtra", type=str, default="FarFieldPointSource_1MeV.inc1.id1.tra")
+parser.add_argument("-k", "--kalmantra", type=str)
+args = parser.parse_args()
+
+if args.standardtra != "":
+    StdFile = args.standardtra
+
+if args.kalmantra != "":
+    KalmanFile = args.kalmantra
 
 #collects all ids from one tra file and stores in a list
 def id_list(filepath):
     return
 
 #iterates through lists and counts how many ids are the same
+def eventMatch(stdList, kalmanList):
+    return 0
 
-print("Current working directory: {0}".format(CWD))
+print(StdFile + \n + KalmanFile)
